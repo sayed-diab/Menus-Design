a95e7bbb7b5bae8cd74228dca356ee3de4d83234
sayed-diab <sayeddebo95@gmail.com>
2025-03-29 11:20:34 +0200

    refactor: simplify component imports in App component using a LazyComponent function

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,69 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-const [Header, Footer] = ['/Header', '/Footer'].map((path) => React.lazy(() => import(`./layout${path}.tsx`)));
-const [Menus, Logos, Banners, Notfound, Socials, Cards, Prints, Sublimation, About, Prices] = ['/Menus', '/Logos', '/Banners', '/Cards', '/Prints', '/Sublimation', '/About', '/Prices', '/Socials', '/404'].map((path) => React.lazy(() => import(`./pages${path}.tsx`)));
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={<Menus />}
-          />
-          <Route
-            path="/branding-logo-design"
-            element={<Logos />}
-          />
-          <Route
-            path="banner"
-            element={<Banners />}
-          />
-          <Route
-            path="/social-media"
-            element={<Socials />}
-          />
-          <Route
-            path="/prints/paper-prints"
-            element={<Prints />}
-          />
-          <Route
-            path="/prints/sublimation-printing"
-            element={<Sublimation />}
-          />
-          <Route
-            path="/prints/card-flair"
-            element={<Cards />}
-          />
-          <Route
-            path="/about-us"
-            element={<About />}
-          />
-          <Route
-            path="/custom-quote"
-            element={<Prices />}
-          />
-          <Route
-            path="*"
-            element={<Notfound />}
-          />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
+
+const LazyComponent = (path: string) => React.lazy(() => import(`./${path}.tsx`));
+
+const components = {
+  Header: LazyComponent('layout/Header'),
+  Footer: LazyComponent('layout/Footer'),
+  Menus: LazyComponent('pages/Menus'),
+  Logos: LazyComponent('pages/Logos'),
+  Banners: LazyComponent('pages/Banners'),
+  Notfound: LazyComponent('pages/404'),
+  Socials: LazyComponent('pages/Socials'),
+  Cards: LazyComponent('pages/Cards'),
+  Prints: LazyComponent('pages/Prints'),
+  Sublimation: LazyComponent('pages/Sublimation'),
+  About: LazyComponent('pages/About'),
+  Prices: LazyComponent('pages/Prices'),
 };
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={<components.Menus />}
+      />
+      <Route
+        path="/branding-logo-design"
+        element={<components.Logos />}
+      />
+      <Route
+        path="/banner"
+        element={<components.Banners />}
+      />
+      <Route
+        path="/social-media"
+        element={<components.Socials />}
+      />
+      <Route
+        path="/prints/paper-prints"
+        element={<components.Prints />}
+      />
+      <Route
+        path="/prints/sublimation-printing"
+        element={<components.Sublimation />}
+      />
+      <Route
+        path="/prints/card-flair"
+        element={<components.Cards />}
+      />
+      <Route
+        path="/about-us"
+        element={<components.About />}
+      />
+      <Route
+        path="/custom-quote"
+        element={<components.Prices />}
+      />
+      <Route
+        path="*"
+        element={<components.Notfound />}
+      />
+    </Routes>
+    <components.Footer />
+  </Router>
+);
 
 export default App;
